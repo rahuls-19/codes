cadf9f2a4e553effd5475ad05108993420331580
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-15 09:56:33 -0800

    Check if a number has bits in alternate pattern

--- bit.c
+++ bit.c
@@ -13,26 +13,25 @@ int main(){
 	return 0;
 }
 int bits(int n){
-	int previous = INT_MAX;
-	int current;
+	
 	while(n>0){
+		int previous = 0;
+	    int current = 0;
 		while(n>0 && n%2==0){
-			n=n/2;
-		}
-		current = 0;
-		while(n>0 && n%2==1){
 			n=n/2;
 			current++;
 		}
-		
-	//	printf("%d . %d --- %d\n",n,current,previous);
-		if(current >=previous){
-	//83	printf(" dcjd");
-		
+		if(current!=1)
 			return 0;
+		current = 0;
+		while(n>0 && n%2==1){
+			n=n/2;
+			previous++;
 		}
+		if(previous!=1)
+			return 0;
 		
-		previous = current;
+
 	}
 	return 1;
 	
