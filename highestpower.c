d5b84deb4031312407bdd4dfee68e8034cccd164
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-18 01:32:24 +0530

    highest power

---
+++ highestpower.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int flag = 0;
+	while(n>0){
+		while( n > 0 && n % 2 == 0)
+		count++;
+		while(n > 0  && n % 2 == 1){
+			count++;
+			temp = count;
+			
+		}
+	}
+	 if (n < 1)
+        printf("0\n");
+    else
+    	printf("%d\n",temp);
+	
+	
+	
+	return 0;
+}
