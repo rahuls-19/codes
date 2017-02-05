788163cb9bec82f764892c9b953c2d93f92edb35
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-05 12:22:01 +0530

    stl c++ : map

---
+++ map.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+	map<int,string> name;
+	name[1] = "rohan";
+	name[2] = "umang";
+	name[3] = "harsh";
+	name[4] = "utkarsh";
+	name[5] = "vinay";
+	name[6] = "akash";
+	name[7] = "saurabh";
+	name[8] = "rahul";
+	for(int i=1;i<9;i++)
+	cout << name[i] << endl;
+	cout << name.size() << endl;
+	name.clear();
+	cout << name.size() << endl;
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
