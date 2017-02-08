8cff28f0ad5db307a83d4e6e5ce612768b196325
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-09 01:11:39 +0530

    modified version of bubblesort

--- bubble.cpp
+++ bubble.cpp
@@ -5,17 +5,22 @@ void swap(int *a,int *b){
 	*b = temp;
 }
 int main(){
-	int n,i,a[10000],j;
+	int n,i,a[10000],j,flag;
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
 	for(i=0;i<n;i++){
+		flag =0;
 		for(j=0;j<n-i-1;j++){
 			if(a[j+1] < a[j]){
 				swap(&a[j],&a[j+1]);
+				flag =1;
 			}
+			
 		}
+		if(flag == 0)
+		break;
 		
 	}
 	for(i=0;i<n;i++){
