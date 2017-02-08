dc09bc7de74f6d3fa701ddb7c01c8e8cc8fb2959
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-09 00:37:10 +0530

    codemonk virat

--- virat.cpp
+++ virat.cpp
@@ -1,21 +1,45 @@
+#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-	long long int t,n,i;
-	scanf("%lld",&t);
-	while(t--){
-		long long int a[1000005],sum=0;
-		scanf("%d",&n);
-		for(i=0;i<n;i++){
-			scanf("%d",&a[i]);
-			sum = sum+a[i];
-			}
-		if((sum-n)%2==1)
-			printf("Virat\n");
-		else
-			printf("Alastair\n");
-		}
 
+int main()
+{
+    	multiset <long long int> s;
+	multiset <long long int> :: iterator it1,it2;
+   long long int n,i,j,m[4][1000006],q;
+    scanf("%lld",&n);
+    for(i=0;i<3;i++){
+        for(j=0;j<n;j++){
+            scanf("%lld",&m[i][j]);
+        }
+    }
+    for(j=0;j<n;j++){
+        long long int key= m[0][j]+m[1][j]+m[2][j];
+        
+        s.insert(key);
+    }
+    
+    scanf("%lld",&q);
+    while(q--){
+        long long int l;
+        scanf("%lld",&l);
+        it1 = s.begin();
+    
+       
+        if(l > s.size())
+            printf("-1\n");
+        else
+        
+           {
+                for(int i = 1;i < l;i++)
+                 it1++;
+                printf("%lld\n",*it1);
+                 s.erase(it1);
+           }
+        
+        
+    }
+    
+    return 0;
+}
 
- return 0;
-}		
