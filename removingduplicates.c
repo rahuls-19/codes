01c2a00f6ae904aefaf8e64f6e68ad3cad79ddb5
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-23 23:25:30 +0530

    removing duplicate alphabet in a word

--- removingduplicates.c
+++ removingduplicates.c
@@ -1,20 +1,25 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<string.h>
+//using namespace std;
 int main()
  {
-    int t,i;
+    int t,i,j;
     char s[10000];
     scanf("%d",&t);
     while(t--){
         scanf("%s",s);
         int count[10000] = {0};
-        for(i=0;i<strlen(s);i++){
+        for(i=0,j=0;i<strlen(s);i++){
             if(count[s[i]] == 0){
                 printf("%c",s[i]);
                 count[s[i]] = 1;
+                s[j] = s[i];
+                j++;
             }
         }
+        s[j] = '\0';
         printf("\n");
+        printf("%s\n",s);
     }
 	return 0;
 }
