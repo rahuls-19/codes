0a90b9715c840e91f97f83ed983b6b458bab1936
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-24 23:15:41 +0530

    creating a class in c++

---
+++ classcreation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class HumanBeing{
+public :
+    void display(){
+    cout << "he is human being"  << endl;
+    }
+
+};
+int main(){
+    HumanBeing he;
+    he.display();
+    return 0;
+
+}
